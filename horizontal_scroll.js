2413729086cbd11a1abdcaeed3a424bb5ae10f59
Goz <gozunam@gmail.com>
2021-02-22 20:16:57 -0600

    Add new features developed in webinar

--- horizontal_scroll.js
+++ horizontal_scroll.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
+    var windowWidth;
+    windowWidth = window.innerWidth;
+    if (windowWidth >= 1024) {
     $('html, body, *').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta * 150);
-    e.preventDefault();
+    this.scrollLeft -= (delta * 125);
     });
+    }
 });
+
+$(window).resize(function() {
+    var windowWidth;
+    windowWidth = window.innerWidth;
+    if (windowWidth <= 1024) {
+      $('html, body, *').mousewheel(function(e, delta) {
+        this.scroll -= (delta * 20);
+      });
+    }  
+    else
+      $('html, body, *').mousewheel(function(e, delta) {
+        this.scrollLeft -= (delta * 20);
+      });
+    console.log('window was resized to ' + windowWidth);
+});
+
